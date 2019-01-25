ac19b19d6bb242225db65b1f4c71a894b8243c34
unknown <cevauo@yahoo.de>
2019-01-25 10:49:21 +0100

    deleted folder

--- flashcards/src/main/java/de/tud/cs/st/constraints/NotNull.java
+++
@@ -1,8 +0,0 @@
-package de.tud.cs.st.constraints;
-
-/**
- * Annotates that a parameter or field must never be null.
- */
-public @interface NotNull {
-
-}
--- flashcards/src/main/java/de/tud/cs/st/constraints/Null.java
+++
@@ -1,8 +0,0 @@
-package de.tud.cs.st.constraints;
-
-/**
- * Annotates that a parameter or field may be null.
- */
-public @interface Null {
-
-}
